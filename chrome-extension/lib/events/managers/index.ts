ff1f4d8e33252b790069ea2f759d05e1a1093c64
Geomario <marm1984@gmail.com>
2025-08-23 15:23:16 +0200

    build: barrel for export in events

---
+++ chrome-extension/lib/events/managers/index.ts
@@ -0,0 +1,10 @@
+// Export all Event Managers.
+
+// Window Manager 
+export { default as WindowEventManager } from './WindowEventManager';
+
+// Tab Manager
+export { default as TabEventManager } from './TabEventManager';
+
+// Domain Manager
+export { default as DomainEventManager } from './DomainEventManager';
