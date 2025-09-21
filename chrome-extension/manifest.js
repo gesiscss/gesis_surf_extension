83885fc37255e78e2531d03bde41ff3738bf727c
Geomario <marm1984@gmail.com>
2025-09-22 00:06:37 +0200

    fix: new manifest creationwithout other pages

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -7,16 +7,16 @@ const isFirefox = process.env.__FIREFOX__ === 'true';
  * After changing, please reload the extension at `chrome://extensions`
  * @type {chrome.runtime.ManifestV3}
  */
-const sidePanelConfig = {
-    side_panel: {
-        default_path: 'sidepanel/index.html',
-    },
-    permissions: !isFirefox ? ['sidePanel'] : [],
-};
+// const sidePanelConfig = {
+//     side_panel: {
+//         default_path: 'sidepanel/index.html',
+//     },
+//     permissions: !isFirefox ? ['sidePanel'] : [],
+// };
 
 const manifest = Object.assign({
     manifest_version: 3,
-    default_locale: 'en',
+    // default_locale: 'en',
     /**
      * if you want to support multiple languages, you can use the following reference
      * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
@@ -24,33 +24,34 @@ const manifest = Object.assign({
     name: 'BrainTrack',
     version: '2.2.1',
     description: 'BrainTrack extension that helps you track your browsing activity', 
-    permissions: ['storage', 'tabs', 'alarms'].concat(sidePanelConfig.permissions),
-    options_page: 'options/index.html',
+    permissions: ['storage', 'tabs', 'alarms'], // .concat(sidePanelConfig.permissions),
+    // options_page: 'options/index.html',
     background: {
         service_worker: 'background.iife.js',
         type: 'module',
     },
     action: {
         default_popup: 'popup/index.html',
-        default_icon: 'icon-34.png',
+        default_icon: 'icon32.png',
     },
     icons: {
-        128: 'icon-128.png',
+        128: 'icon128.png',
     },
-    content_scripts: [{
-            matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-            js: ['content/index.iife.js'],
-        },
-        {
-            matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-            css: ['content.css'], // public folder
-        },
-    ],
-    devtools_page: 'devtools/index.html',
+    // content_scripts: [{
+    //         matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //         js: ['content/index.iife.js'],
+    //     },
+    //     {
+    //         matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //         css: ['content.css'], // public folder
+    //     },
+    // ],
+    // devtools_page: 'devtools/index.html',
     web_accessible_resources: [{
         resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
         matches: ['*://*/*'],
     }, ],
-}, !isFirefox && { side_panel: {...sidePanelConfig.side_panel } }, );
+// }, !isFirefox && { side_panel: {...sidePanelConfig.side_panel } }, );
+}, !isFirefox && { /* ...sidePanelConfig */ }, );
 
 export default manifest;
