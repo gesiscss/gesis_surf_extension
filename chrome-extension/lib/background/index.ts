0a162597b3158122635746fad63777c592168a40
Geomario <marm1984@gmail.com>
2025-11-22 12:34:46 +0100

    refactor: integrating message manager and clean code to prod

--- chrome-extension/lib/background/index.ts
+++ chrome-extension/lib/background/index.ts
@@ -22,121 +22,31 @@ runtime.onInstalled.addListener(async(details: Runtime.OnInstalledDetailsType) =
     await authService.checkAuthentication();
 });
 
-runtime.onMessage.addListener(
-    (
-        message,
-        sender,
-        sendResponse : (response: MessageResponse) => void
-    ) => {
-        console.log('[background] onMessage', message);
-        void messageHandler.handleAuthSuccess(
-            message,
-            sendResponse
-        );
+// Listen for incoming messages
+runtime.onMessage.addListener((
+    message: unknown,
+    sender: Runtime.MessageSender,
+    sendResponse: (response: MessageResponse) => void
+    ): true => {
+        messageHandler.handleMessage(message, sender, sendResponse);
         return true;
-    }
-);
+    });
 
-// import { alarms, runtime, storage} from 'webextension-polyfill'
-// import {getCurrentTab, startListeners} from './helpers/tabs'
-// import { syncData } from './controllers/core'
-// import { demoDB, getDomainIdByTabWindowNum } from './db/dblocal'
-// import { setCick, setScroll } from './services/syncService'
-// import { startAlarmAll, startHeartbeat } from './services/heartbeat'
-// import { initPrivateMode, setPrivateMode } from './services/privateMode'
-// import { checkVersions, installedVersion } from './helpers/config'
-// import { getHostVersion, getHosts } from './middleware/hosts'
-// import { getDomainsByWinnumLocal } from './helpers/offline'
-// type Message = {
-//   from: string
-//   info: string
-//   action: string,
-//   tab:number,
-//   to:string,
-//   meta:object
-// }
+// Flush pending messages before unloading
+runtime.onSuspend.addListener(() => {
+    console.log('[background] onSuspend - flushing pending messages if any');
+    messageHandler.flushPendingEvents().catch((error) => {
+        console.error('[background] Error flushing pending events on suspend:', error);
+    });
+});
 
-
-// export async function init() {
-
-//   runtime.onMessage.addListener(async (message: Message, sender) => {
-//     if (message.action === 'pageLoaded'){
-//        syncData(message.info,sender,message.meta);
-//     }
-
-//     if (message.action === 'setToken'){
-//      await storage.local.set({ "WTG_User": message.meta});
-//      await getHostVersion().then(data=>data ? getHosts() : null);
-//     } 
-
-//     if(message.action == 'scrollEnd'){
-//       await setScroll(sender.tab,message.info,message.meta)
-//     }
-
-//     if(message.action == 'clickContent'){
-//       await setCick(sender.tab,message.info,message.meta)
-//     }
-    
-
-//     if(message.action == 'login'){
-//         await getCurrentTab();
-//         await installedVersion();
-//     }
-
-//   });
-
-//   alarms.onAlarm.addListener((alarm) => {
-//     if(alarm.name=='privatemode'){
-//       setPrivateMode(false);
-//     }
-//   });
-
-//   startListeners();
-//   initPrivateMode();
-
-// }
-
-// runtime.onStartup.addListener(()=>{
-//   init().then(async () => {
-//     console.log('[background] Startup ');
-//     startHeartbeat();
-//     await getHostVersion().then(data=>data ? getHosts() : null);
-//     getCurrentTab();
-//     checkVersions();
-//     getDomainsByWinnumLocal();
-//     startAlarmAll(); 
-//   })
-// });
-
-// runtime.onInstalled.addListener(() => {
-//   init().then(async () => {
-//     console.log('[background] loaded ');
-//     startHeartbeat();
-//     demoDB();
-//     installedVersion();
-//     startAlarmAll(); 
-//   })
-// });
-
-// runtime.onUpdateAvailable.addListener(()=>{
-//   init().then(async () => {
-//     console.log('[background] loaded ');
-//     startHeartbeat();
-//     getCurrentTab();
-//     checkVersions();
-//     getDomainsByWinnumLocal();
-//     getHostVersion().then((host)=>{
-//       host ? getHosts() : null
-//     });
-//     startAlarmAll();  
-//   })
-// });
-
-// runtime.onSuspendCanceled.addListener(() => {
-//   console.log('Canceled');
-// });
-
-// runtime.onSuspend.addListener(() => {
-//   console.log('Suspend');
-//   startHeartbeat();
-// })
+// Flush on unload
+if (typeof self !== 'undefined') {
+    self.addEventListener('unload', () => {
+        console.log('[background] unload event - flushing pending messages if any');
+        messageHandler.flushPendingEvents().catch((error) => {
+            console.error('[background] Error flushing pending events on unload:', error);
+        });
+    });
+}
+console.log('[background] Background script initialization complete');
