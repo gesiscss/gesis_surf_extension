f9f8e71409235a3b00407e732d40de370a294845
Geomario <marm1984@gmail.com>
2025-11-27 16:23:05 +0100

    refactor: deleting background class based

--- chrome-extension/lib/background/types.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService, PrivateModeService } from "../services";
-import { MessageHandler } from "../messages";
-
-export interface ServiceContainer {
-    authService: AuthService;
-    privateModeService: PrivateModeService;
-    messageHandler: MessageHandler;
-}
