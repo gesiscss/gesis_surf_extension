9199c5a1579eea366c2869dc578940b3f6177667
Geomario <marm1984@gmail.com>
2025-11-27 23:06:02 +0100

    refactor: adding the new content script api handler

--- chrome-extension/lib/handlers/clients/index.ts
+++ chrome-extension/lib/handlers/clients/index.ts
@@ -6,3 +6,5 @@ export { default as WindowHandler } from './WindowHandler';
 export { default as TabHandler } from './TabHandler';
 // Domain Handler
 export { default as DomainHandler } from './DomainHandler';
+// Content Script Handler
+export { default as ContentScriptHandler } from './ContentScriptHandler';
