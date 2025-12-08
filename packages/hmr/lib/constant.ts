5b2756034233a9d75cb26e6089130223f05c0ad3
Geomario <marm1984@gmail.com>
2025-12-08 21:06:46 +0100

    refactor: single entry point to URLs API

--- packages/hmr/lib/constant.ts
+++ packages/hmr/lib/constant.ts
@@ -1,19 +1,30 @@
 export const LOCAL_RELOAD_SOCKET_PORT = 8081;
 export const LOCAL_RELOAD_SOCKET_URL = `ws://localhost:${LOCAL_RELOAD_SOCKET_PORT}`;
 
+const STG_URL = 'https://staging.surfcollect.gesis.org/api';
+const PROD_URL = 'https://surfcollect.gesis.org/api';
+const LOCAL_URL = 'http://localhost:8000/api';
+
+
+const getApiUrl = (): string => LOCAL_URL;
 
 // API Configuration & environment switching
 export const API_CONFIG = {
-    BASE_URL: 'https://surfcollect.gesis.org/api',
-    STG_URL: 'https://staging.surfcollect.gesis.org/api',
-    LOCAL_URL: 'http://localhost:8000/api',
+    BASE_URL: getApiUrl(),
+    STG_URL,
+    PROD_URL,
+    LOCAL_URL,
+    LOGGING_BASE_URL: getApiUrl(),
+    GESIS_BASE_URL: getApiUrl(),
     ENDPOINTS: {
         USER_TOKEN: '/user/token/',
         USER_ME: '/user/me/',
         GLOBAL_SESSION: '/session/',
         CLICK: '/clicks/',
-        SCROLL: '/scroll/scroll/',
+        SCROLL: '/scrolls/',
         TAB: '/tab/tabs/',
         DOMAIN: '/domain/domains/',
+        HOST: '/host/hosts/',
+        HOST_TASK: '/host/task-result/',
     }
 } as const;
